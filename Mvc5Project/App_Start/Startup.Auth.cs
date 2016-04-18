d1256e142750f4def4eb439afa26aecbac6ab672
DevsOne <mehmetkenis@hotmail.com>
2016-04-18 04:42:36 +0300

    Update Startup.Auth.cs

--- Mvc5Project/App_Start/Startup.Auth.cs
+++ Mvc5Project/App_Start/Startup.Auth.cs
@@ -78,15 +78,34 @@ namespace Mvc5Project
 
             #endregion Microsoft
 
-            #region Twitter
+             #region Twitter
             var twitterAuthenticationOptions = new TwitterAuthenticationOptions()
             {
                 ConsumerKey = ConfigurationManager.AppSettings["TwtI"],
-                ConsumerSecret = ConfigurationManager.AppSettings["TwtS"]
+                ConsumerSecret = ConfigurationManager.AppSettings["TwtS"],
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(
+                new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+                "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
+                "B13EC36903F8BF4701D498261A0802EF63642BC3" // DigiCert High Assurance EV Root CA
+                })
             };
             app.UseTwitterAuthentication(twitterAuthenticationOptions);
 
             #endregion Twitter
+
+            #region LinkedIn
+
+            app.UseLinkedInAuthentication(
+                clientId: "77g2s7wgbminls",
+                clientSecret: "20S5G0D0Lh40IRKW"
+                );
+            #endregion
 
             #region Facebook
             var facebookAuthenticationOptions = new FacebookAuthenticationOptions()
